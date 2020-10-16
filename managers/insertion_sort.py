18e849b3d14618adc881f29a0c804e19413c4b46
QdKyi <denroy777@gmail.com>
2020-10-16 08:33:39 +0300

    Actually made some kind of bubble instead fo insertion lol)

--- managers/insertion_sort.py
+++ managers/insertion_sort.py
@@ -10,14 +10,15 @@ class InsertionSort:
 
         for i in range(1, len(given_array)):
             current_value = given_array[i]
-            current_position = i
+            current_position = i - 1
 
             while current_position > 0 and compare_function(given_array[current_position - 1], current_value):
-                swap(given_array, current_position, current_position - 1)
+                given_array[current_position + 1] = given_array[current_position]
                 current_position = current_position - 1
 
                 self.counter_of_swap_operations += 1
                 self.counter_of_comparison_operations += 2
+            given_array[current_position + 1] = current_value
 
         print(f'InsertionSort:\n'
               f'Number of compares = {self.counter_of_comparison_operations}\n'
