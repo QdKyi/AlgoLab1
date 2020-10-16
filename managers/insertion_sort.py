4ac29c8de9b43ffbbdd7e45028cc5c32b3b4f2c4
QdKyi <denroy777@gmail.com>
2020-10-16 08:57:51 +0300

    Reversed changes. I dunno what i've done before(

--- managers/insertion_sort.py
+++ managers/insertion_sort.py
@@ -10,15 +10,13 @@ class InsertionSort:
 
         for i in range(1, len(given_array)):
             current_value = given_array[i]
-            current_position = i - 1
+            current_position = i
 
             while current_position > 0 and compare_function(given_array[current_position - 1], current_value):
-                given_array[current_position + 1] = given_array[current_position]
-                current_position = current_position - 1
+                swap(given_array, current_position, current_position - 1)
 
                 self.counter_of_swap_operations += 1
                 self.counter_of_comparison_operations += 2
-            given_array[current_position + 1] = current_value
 
         print(f'InsertionSort:\n'
               f'Number of compares = {self.counter_of_comparison_operations}\n'
